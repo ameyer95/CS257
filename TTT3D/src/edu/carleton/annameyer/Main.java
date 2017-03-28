717bfa69ae35ca01a45269c4bcc7e7f10d8d9f39
Anna Meyer <annameyer@eduroam-169-11.dyn.carleton.edu>
2017-03-27 21:41:20 -0500

    added the main file with hello world

--- TTT3D/src/edu/carleton/annameyer/Main.java
+++ TTT3D/src/edu/carleton/annameyer/Main.java
@@ -3,6 +3,8 @@ package edu.carleton.annameyer;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        for(int k = 0; k<10;k++){
+            System.out.println("Hello, world!");
+        }
     }
 }
