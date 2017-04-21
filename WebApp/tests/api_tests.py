21bf04759ed7db2e83035814ec32f99bfba1b5fb
comminsp <comminsp@carleton.edu>
2017-04-21 14:54:55 -0500

    Made files for tests

---
+++ WebApp/tests/api_tests.py
@@ -0,0 +1,19 @@
+'''
+api_tests.py
+Anna Meyer and Patty Commins, 21 May 2017
+'''
+
+import unittest
+
+class apiTester(unittest.TestCase):
+    def setUp(self):
+
+    def tearDown(self):
+
+    #Tests to see if our input characters, get spells api query works
+    #with Ron Weasley
+    def testCharToSpells(self):
+      apiReturn =
+
+if _name_ == '_main_':
+    unittest.main()
