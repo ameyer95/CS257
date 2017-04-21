8276a71c8bd7ae73a1d4970588a008cf5b6b4256
comminsp <comminsp@carleton.edu>
2017-04-21 16:37:18 -0500

    Added another test

--- WebApp/tests/api_tests.py
+++ WebApp/tests/api_tests.py
@@ -14,20 +14,43 @@ class apiTester(unittest.TestCase):
 
     def tearDown(self):
 
-    '''Tests to see if our input characters, get spells api query works
+    '''Tests to see if our input: characters, get: list of spells api query works
     with Ron Weasley'''
     def testCharToSpells(self):
+        #Should change spell_char in url to just characters?
         url = "http://standardbookofspells.com/spell_char/Ron_Weasley/"
         data_from_api = urllib.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
         spell_list = json.loads(string_from_server)
-        expected_list = ['accio", "caveinimicum", "expelliarmus", "relashio", "riddikulus", "silencio", "tergeo", "wingardiumleviosa"]
+        expected_list = ["accio", "caveinimicum", "expelliarmus", "relashio", "riddikulus", "silencio", "tergeo",
+                         "wingardiumleviosa"]
         #Is object stuff going to matter?
         self.assertEqual(spell_list, expected_list)
         #If so, we can do this
         self.assertEqual(spell_list.size(), expected_list.size())
         for spell in spell_list:
             self.assertTrue(expected_list.contains(spell))
+
+    '''Tests whether our input: book, get: list of spells api query works with Deathly Hallows'''
+    def bookToSpells:
+        #Should change spell_book in url to just book?
+        url = 'http://standardbookofspells.com/spell_book/Deathly_Hallows/'
+        data_from_api = urllib.urlopen(url).read()
+        string_from_server = data_from_api.decode('utf-8')
+        spell_list = json.loads(string_from_server)
+        expected_list = ["avadakedavra", "caveinimicum", "confringo", "confundus", "crucio", "deprimo", "diffindo", "duro",
+                         "engorgio", "erecto", "expectopatronum", "expelliarmus", "expulso", "fidelius","geminio", "glisseo",
+                         "homenumrevelio", "impedimienta", "imperio", "impervius", "levicorpus", "liberacorpus", "lumos",
+                         "meteolojinxrecanto", "muffliato", "nox", "obliviate", "obscuro", "petrificustotalus",
+                         "piertotumlocomotor", "protego", "protegototalum", "reducio", "relashio", "reparo", "repellomuggletum",
+                         "salviohexia", "sectumsepra", "stuepfy", "tergeo", "wingardiumleviosa"]
+        # Is object stuff going to matter?
+        self.assertEqual(spell_list, expected_list)
+        # If so, we can do this
+        self.assertEqual(spell_list.size(), expected_list.size())
+        for spell in spell_list:
+            self.assertTrue(expected_list.contains(spell))
+
 
 if _name_ == '_main_':
     unittest.main()
