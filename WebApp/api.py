647e07f43ea9d9d0fb3a788fdd11955bdea135a0
Anna Meyer <annameyer@eduroam-169-11.dyn.carleton.edu>
2017-04-26 21:15:07 -0500

    adding api.py and changes to csv files

--- WebApp/api.py
+++ WebApp/api.py
@@ -1,29 +1,318 @@
 #api.py
 #!/usr/bin/env python3
 '''
-    example_flask_app.py
-    Jeff Ondich, 22 April 2016
+    api.py
+    Patty Commins and Anna Meyer, 26 April 2016
 
-    A slightly more complicated Flask sample app than the
-    "hello world" app found at http://flask.pocoo.org/.
+    Runs the API for our Harry Potter spell database
 '''
+
+import sys
 import flask
 import json
+import config
+import psycopg2
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__,static_folder='static',template_folder = 'template')
 
-@app.route('/')
-def hello():
-    return 'Hello, Citizen of CS257.'
+def _fetch_all_rows_for_query(query):
+    '''
+       Returns a list of rows obtained from the database by the specified SQL
+       query. If the query fails for any reason, an empty list is returned.
 
-@app.route('/authors/<author>')
-def get_author(author):
-    ''' What a dopey function! But it illustrates a Flask route with a parameter. '''
-    if author == 'Twain':
-        author_dictionary = {'last_name':'Twain', 'first_name':'Mark'}
-    else:
-        author_dictionary = {'last_name':'McBozo', 'first_name':'Bozo'}
-    return json.dumps(author_dictionary)
+       Note that this is not necessarily the right error-handling choice. Would users
+       of the API like to know the nature of the error? Do we as API implementors
+       want to share that information? There are many considerations to balance.
+       '''
+    try:
+        connection = psycopg2.connect(database=config.database, user=config.user, password=config.password)
+    except Exception as e:
+        print('Connection error:', e, file=sys.stderr)
+        return []
+
+    rows = []
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()  # This can be trouble if your query results are really big.
+    except Exception as e:
+        print('Error querying database:', e, file=sys.stderr)
+
+        connection.close()
+        return rows
+
+        # related to user interface only
+        # determines what programs can call your API
+
+
+@app.after_request
+def set_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
+
+@app.route('/characters/<character_name>')
+def get_char_id_by_name(character_name):
+    ''' 
+    Given a character's first or last name (or both), return the
+    ID number associated with that character.
+    If character cannot be found in our database, will return an
+    empty list.
+    '''
+    query = '''SELECT id, first_name, last_name 
+                FROM characters
+                WHERE UPPER(last_name) LIKE UPPER('%{0}%)
+                or UPPER(first_name) LIKE UPPER('%{0}%)
+                or UPPER(first_name + ' ' + last_name) LIKE UPPER('%')
+                ORDER BY last_name, first_name'''.format(character_name)
+
+    character_list = []
+    for row in _fetch_all_rows_for_query(query):
+        url = flask.url_for('get_character_by_id', character_id = row[0], _external = True)
+        character = {'character_id':row[0], 'first_name':row[1], 'last_name':row[2],
+                     'url':url}
+        character_list.append(character)
+
+    return json.dumps(character_list)
+
+@app.route('characters/<character_id>')
+def get_character_by_id(character_id):
+    ''' 
+    Returns the character associated with the specified ID.
+    The character resource will be represented as a JSON dictionary
+    with keys 'first_name' (string), 'last_name'(string), 
+    'character_id' (int), and 'url' (string). The URL can be used to access
+     this character in the future.
+     '''
+    query = '''SELECT id, first_name, last_name
+                FROM characters WHERE id = {0}'''.format(character_id)
+
+    rows = _fetch_all_rows_for_query(query)
+    if len(rows) > 0:
+        row = rows[0]
+        url = flask.url_for('get_char_by_id', character_id = row[0], _external = True)
+        character = {'character_id':row[0], 'first_name':row[1], 'last_name':row[2],
+                     'url':url}
+        return json.dumps(character)
+
+    return json.dumps({})
+
+@app.route('/spells/<spell_id>')
+def get_spell_by_id(spell_id):
+    ''' 
+    Returns the spell associated with the specified ID.
+    The spell resource will be represented as a JSON dictionary
+    with keys 'spell_name' (string), 'type_id'(int), 
+    'spell_effect' (string), 'spell_id' (int) and 'url' (string). The URL can be used to access
+     this spell in the future.
+     '''
+    query = '''SELECT id, spell_name, type_id, spell_effect
+                FROM spell_names WHERE id = {0}'''.format(spell_id)
+
+    rows = _fetch_all_rows_for_query(query)
+    if len(rows) > 0:
+        row = rows[0]
+        url = flask.url_for('get_spell_by_id', spell_id = row[0], _external = True)
+        spell = {'spell_id':row[0], 'spell_name':row[1], 'type_id':row[2],
+                     'spell_effect':row[3], 'url':url}
+        return json.dumps(spell)
+
+    return json.dumps({})
+
+@app.route('/spells/<spell_name>')
+def get_spell_by_name(spell_name):
+    ''' 
+    Given a spell name, return the
+    ID number associated with that spell.
+    If the spell cannot be found in our database, will return an
+    empty list.
+    '''
+    query = '''SELECT id, spell_name, type_id, spell_effect 
+                FROM spell_names
+                WHERE UPPER(spell_name) LIKE UPPER('%{0}%)
+                ORDER BY spell_name'''.format(spell_name)
+
+    spell_list = []
+    for row in _fetch_all_rows_for_query(query):
+        url = flask.url_for('get_spell_by_id', spell_id = row[0], _external = True)
+        spell = {'spell_id':row[0], 'spell_name':row[1], 'type_id':row[2],
+                     'spell_effect':row[3], 'url':url}
+        spell_list.append(spell)
+
+    return json.dumps(spell_list)
+
+@app.route('/characters/<character_id>')
+def get_spells_by_character(character_id):
+    ''' 
+    Returns a list of all the spells that a given 
+    character used. 
+    '''
+    query = '''SELECT spell_names.id, spells_names.spell_name, spell_names.type_id, spell_names.spell_effect
+                FROM spell_names, instances, characters
+                WHERE spell_names.id = instances.spell_id
+                AND characters.id = instances.char_id
+                AND characters.id = {0}
+                ORDER BY spell_names.spell_name'''.format({0})
+
+    spell_list = []
+    for row in _fetch_all_rows_for_query(query):
+        url = flask.url_for('get_spell_by_id', spell_id = row[0], _external = True)
+        spell = {'spell_id':row[0], 'spell_name':row[1], 'type_id':row[2], 'spell_effect':row[3],
+                 'url':url}
+        spell_list.append(spell)
+
+    return json.dumps(spell_list)
+
+@app.route('/characters/<character_name>')
+def get_spells_by_character_name(character_name):
+    '''
+    Returns a list of all the spells that a given character used.
+    Allows the user to input the character's name as a string, then 
+    finds the associated ID and calls get_spells_by_character
+    '''
+    character_id = get_char_id_by_name(character_name)
+    return get_spells_by_character(character_id)
+
+@app.route('/books/<book_id>')
+def get_book_by_id(book_id):
+    ''' 
+    Returns the book associated with the specified ID.
+    The book resource will be represented as a JSON dictionary
+    with keys 'book_name' (string), 'book_id' (int) and 'url' (string). The URL can be used to access
+     this book in the future.
+     '''
+    query = '''SELECT id, book_name
+                FROM books WHERE id = {0}'''.format(book_id)
+
+    rows = _fetch_all_rows_for_query(query)
+    if len(rows) > 0:
+        row = rows[0]
+        url = flask.url_for('get_book_by_id', book_id = row[0], _external = True)
+        book = {'book_id':row[0], 'book_name':row[1], 'url':url}
+        return json.dumps(book)
+
+    return json.dumps({})
+
+@app.route('/books/<book_name>')
+def get_book_id_by_name(book_name):
+    ''' 
+    Returns the book idassociated with the specified ID.
+    The book resource will be represented as a JSON dictionary
+    with keys 'book_name' (string), 'book_id' (int) and 'url' (string). The URL can be used to access
+     this book in the future.
+     '''
+    query = '''SELECT id, book_name
+                FROM books WHERE book_name = {0}'''.format(book_name)
+
+    rows = _fetch_all_rows_for_query(query)
+    if len(rows) > 0:
+        row = rows[0]
+        url = flask.url_for('get_book_by_id', book_id = row[0], _external = True)
+        book = {'book_id':row[0], 'book_name':row[1], 'url':url}
+        return json.dumps(book)
+
+    return json.dumps({})
+
+
+@app.route('/books/<book_id>')
+def get_spells_by_book(book_id):
+    ''' 
+    Returns a list of all the spells used in a given book. 
+    '''
+    query = '''SELECT spell_names.id, spells_names.spell_name, spell_names.type_id, spell_names.spell_effect
+                FROM spell_names, instances, books
+                WHERE spell_names.id = instances.spell_id
+                AND books.id = instances.book_id
+                AND books.id = {0}
+                ORDER BY spell_names.spell_name'''.format({0})
+
+    spell_list = []
+    for row in _fetch_all_rows_for_query(query):
+        url = flask.url_for('get_spell_by_id', spell_id=row[0], _external=True)
+        spell = {'spell_id': row[0], 'spell_name': row[1], 'type_id': row[2], 'spell_effect': row[3],
+                 'url': url}
+        spell_list.append(spell)
+
+    return json.dumps(spell_list)
+
+@app.route('/books/<book_name>')
+def get_spells_by_book_name(book_name):
+    '''
+    Returns a list of all the spells used in a given book.
+    Allows the user to input the book name (as a string), then converts
+    that string to the book ID and calls the get_spells_by_book method
+    '''
+
+    book_id = get_book_id_by_name(book_name)
+    return get_spells_by_book(book_id)
+
+@app.route('/spells/<spell_id>')
+def get_characters_by_spell(spell_id):
+    '''
+    Returns a list of all characters who used the given spell.
+    '''
+    query = '''SELECT characters.id, characters.first_name, characters.last_name
+                FROM spell_names, instances, characters
+                WHERE spell_names.id = instances.spell_id
+                AND characters.id = instances.char_id
+                AND spell_names.id = {0}
+                ORDER BY characters.last_name, characters.first_name'''.format({0})
+
+    character_list = []
+    for row in _fetch_all_rows_for_query(query):
+        url = flask.url_for('get_characters_by_id', character_id=row[0], _external=True)
+        character = {'character_id': row[0], 'first_name': row[1], 'last_name': row[2],
+                 'url': url}
+        character_list.append(character)
+
+    return json.dumps(character_list)
+
+@app.route('/spells/<spell_name>')
+def get_characters_by_spell_name(spell_name):
+    ''' 
+    Returns a list of all the characters who used a given spell. 
+    Allows the user to input the spell name as a string, then finds the associated ID and
+    calls get_characters_by_spell using that ID'''
+    spell_id = get_spell_by_name(spell_name)
+    return get_characters_by_spell(spell_id)
+
+@app.route('/spells/<spell_id>')
+def get_spell_count(spell_id):
+    ''' 
+    Returns the number of time a given spell (input as an ID number) was
+    used across all books
+    '''
+    query = '''SELECT COUNT(*)
+                FROM instances
+                WHERE spell_id = {0}'''.format({0})
+
+    for row in _fetch_all_rows_for_query(query):
+        url = flask.url_for('get_spell_count', spell_id={0}, _external=True)
+        count = row[0]
+    return json.dumps(count)
+
+@app.route('/spells/<spell_name')
+def get_spell_count_by_name(spell_name):
+    ''' 
+    Return the number of time a given spell was used across all books.
+    Allows the user to input the spell name as a string, then finds the associated
+    ID number and calls the get_spell_count method.
+    '''
+    spell_id = get_spell_by_name(spell_name)
+    return get_spell_count(spell_id)
+
+@app.route('/help')
+def help():
+    rule_list = []
+    for rule in app.url_map.iter_rules():
+        rule_text = rule.rule.replace('<', '&lt;').replace('>','&gt;')
+        rule_list.append(rule_text)
+    return json.dump(rule_list)
 
 if __name__ == '__main__':
-    app.run(host='thacker.mathcs.carleton.edu', port=5127)
+    if len(sys.argv) != 3:
+        print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
+        exit()
+
+    host = sys.argv[1]
+    port = sys.argv[2]
+    app.run(host=host, port=port)
