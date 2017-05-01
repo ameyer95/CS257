598150a027be80c01cd5d066decf53bce3e869f5
comminsp <comminsp@cmc306-04.mathcs.carleton.edu>
2017-05-01 14:12:43 -0500

    added config file and made changes to api.py

--- WebApp/api.py
+++ WebApp/api.py
@@ -50,6 +50,52 @@ def set_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
+@app.route('/characters')
+def get_all_characters():
+    '''
+    Return a list of all characters in the database.
+    '''
+    query = '''SELECT id, first_name, last_name
+                FROM characters
+                ORDER BY last_name, first_name'''.format()
+    character_list = []
+    for row in _fetch_all_rows_for_query(query):
+        url = flask.url_for('get_character_by_id', character_id = row[0], _external = True)
+        character = {'character_id':row[0], 'first_name':row[1], 'last_name':row[2], 'url':url}
+        character_list.append(character)
+    return json.dumps(character_list)
+
+@app.route('/spells')
+def get_all_spells():
+    '''
+    Return a list of all spells in the database.
+    '''
+    query = '''SELECT id, spell_name, spell_effect
+                FROM spells
+                ORDER BY spell_name'''.format()
+    spell_list = []
+    for row in _fetch_all_rows_for_query(query):
+        url = flask.url_for('get_spell_by_id', character_id = row[0], _external = True)
+        spell = {'spell_id':row[0], 'spell_name':row[1], 'effect':row[2], 'url':url}
+        spell_list.append(spell)
+    return json.dumps(spell_list)
+
+@app.route('/books')
+def get_all_books():
+    '''
+    Return a list of all books in the database.
+    '''
+    query = '''SELECT id, name
+                FROM books
+                ORDER BY id'''.format()
+    book_list = []
+    for row in _fetch_all_rows_for_query(query):
+        url = flask.url_for('get_book_by_id', book_id = row[0], _external = True)
+        book = {'book_id':row[0], 'title':row[1], 'url':url}
+        book_list.append(book)
+    return json.dumps(book_list)
+
+
 @app.route('/characters/<character_name>')
 def get_char_id_by_name(character_name):
     ''' 
@@ -74,7 +120,7 @@ def get_char_id_by_name(character_name):
 
     return json.dumps(character_list)
 
-@app.route('characters/<character_id>')
+@app.route('/characters/char_id/<character_id>')
 def get_character_by_id(character_id):
     ''' 
     Returns the character associated with the specified ID.
@@ -89,14 +135,14 @@ def get_character_by_id(character_id):
     rows = _fetch_all_rows_for_query(query)
     if len(rows) > 0:
         row = rows[0]
-        url = flask.url_for('get_char_by_id', character_id = row[0], _external = True)
+        url = flask.url_for('get_character_by_id', character_id = row[0], _external = True)
         character = {'character_id':row[0], 'first_name':row[1], 'last_name':row[2],
                      'url':url}
         return json.dumps(character)
 
     return json.dumps({})
 
-@app.route('/spells/<spell_id>')
+@app.route('/spells/spell_id/<spell_id>')
 def get_spell_by_id(spell_id):
     ''' 
     Returns the spell associated with the specified ID.
@@ -105,7 +151,7 @@ def get_spell_by_id(spell_id):
     'spell_effect' (string), 'spell_id' (int) and 'url' (string). The URL can be used to access
      this spell in the future.
      '''
-    query = '''SELECT id, incantation, type_id, purpose
+    query = '''SELECT id, incantantation, type_id, purpose
                 FROM spells WHERE id = {0}'''.format(spell_id)
 
     rows = _fetch_all_rows_for_query(query)
@@ -126,10 +172,10 @@ def get_spell_by_name(spell_name):
     If the spell cannot be found in our database, will return an
     empty list.
     '''
-    query = '''SELECT id, incantation, type_id, purpose 
+    query = '''SELECT id, incantantation, type_id, purpose 
                 FROM spells
-                WHERE UPPER(incantation) LIKE UPPER('%{0}%)
-                ORDER BY incantation'''.format(spell_name)
+                WHERE UPPER(incantantation) LIKE UPPER('%{0}%')
+                ORDER BY incantantation'''.format(spell_name)
 
     spell_list = []
     for row in _fetch_all_rows_for_query(query):
@@ -140,18 +186,18 @@ def get_spell_by_name(spell_name):
 
     return json.dumps(spell_list)
 
-@app.route('/characters/<character_id>')
+@app.route('/characters_spells/char_id/<character_id>')
 def get_spells_by_character(character_id):
     ''' 
     Returns a list of all the spells that a given 
     character used. 
     '''
-    query = '''SELECT spells.id, spells.incantation, spells.type_id, spells.purpose
+    query = '''SELECT spells.id, spells.incantantation, spells.type_id, spells.purpose
                 FROM spells, instances, characters
                 WHERE spells.id = instances.spell_id
                 AND characters.id = instances.character_id
                 AND characters.id = {0}
-                ORDER BY spells.incantation'''.format({0})
+                ORDER BY spells.incantantation'''.format({0})
 
     spell_list = []
     for row in _fetch_all_rows_for_query(query):
@@ -162,7 +208,7 @@ def get_spells_by_character(character_id):
 
     return json.dumps(spell_list)
 
-@app.route('/characters/<character_name>')
+@app.route('/characters_spells/<character_name>')
 def get_spells_by_character_name(character_name):
     '''
     Returns a list of all the spells that a given character used.
@@ -172,7 +218,7 @@ def get_spells_by_character_name(character_name):
     character_id = get_char_id_by_name(character_name)
     return get_spells_by_character(character_id)
 
-@app.route('/books/<book_id>')
+@app.route('/books/book_id/<book_id>')
 def get_book_by_id(book_id):
     ''' 
     Returns the book associated with the specified ID.
@@ -195,7 +241,7 @@ def get_book_by_id(book_id):
 @app.route('/books/<book_name>')
 def get_book_id_by_name(book_name):
     ''' 
-    Returns the book idassociated with the specified ID.
+    Returns the book id associated with the specified ID.
     The book resource will be represented as a JSON dictionary
     with keys 'book_name' (string), 'book_id' (int) and 'url' (string). The URL can be used to access
      this book in the future.
@@ -213,17 +259,17 @@ def get_book_id_by_name(book_name):
     return json.dumps({})
 
 
-@app.route('/books/<book_id>')
+@app.route('/books_spells/book_id/<book_id>')
 def get_spells_by_book(book_id):
     ''' 
     Returns a list of all the spells used in a given book. 
     '''
-    query = '''SELECT spells.id, spells.incantation, spells.type_id, spells.purpose
+    query = '''SELECT spells.id, spells.incantantation, spells.type_id, spells.purpose
                 FROM spells, instances, books
                 WHERE spells.id = instances.spell_id
                 AND books.id = instances.book_id
                 AND books.id = {0}
-                ORDER BY spells.incantation'''.format({0})
+                ORDER BY spells.incantantation'''.format({0})
 
     spell_list = []
     for row in _fetch_all_rows_for_query(query):
@@ -234,7 +280,7 @@ def get_spells_by_book(book_id):
 
     return json.dumps(spell_list)
 
-@app.route('/books/<book_name>')
+@app.route('/books_spells/<book_name>')
 def get_spells_by_book_name(book_name):
     '''
     Returns a list of all the spells used in a given book.
@@ -245,7 +291,7 @@ def get_spells_by_book_name(book_name):
     book_id = get_book_id_by_name(book_name)
     return get_spells_by_book(book_id)
 
-@app.route('/spells/<spell_id>')
+@app.route('/characters_spell/spell_id/<spell_id>')
 def get_characters_by_spell(spell_id):
     '''
     Returns a list of all characters who used the given spell.
@@ -259,14 +305,14 @@ def get_characters_by_spell(spell_id):
 
     character_list = []
     for row in _fetch_all_rows_for_query(query):
-        url = flask.url_for('get_characters_by_id', character_id=row[0], _external=True)
+        url = flask.url_for('get_character_by_id', character_id=row[0], _external=True)
         character = {'character_id': row[0], 'first_name': row[1], 'last_name': row[2],
                  'url': url}
         character_list.append(character)
 
     return json.dumps(character_list)
 
-@app.route('/spells/<incantation>')
+@app.route('/characters_spell/<incantation>')
 def get_characters_by_spell_name(incantation):
     ''' 
     Returns a list of all the characters who used a given spell. 
@@ -275,7 +321,7 @@ def get_characters_by_spell_name(incantation):
     spell_id = get_spell_by_name(incantation)
     return get_characters_by_spell(spell_id)
 
-@app.route('/spells/<spell_id>')
+@app.route('/spell_count/spell_id/<spell_id>')
 def get_spell_count(spell_id):
     ''' 
     Returns the number of time a given spell (input as an ID number) was
@@ -286,11 +332,11 @@ def get_spell_count(spell_id):
                 WHERE spell_id = {0}'''.format({0})
 
     for row in _fetch_all_rows_for_query(query):
-        url = flask.url_for('get_spell_count', spell_id={0}, _external=True)
+        url = flask.url_for('get_spell_by_id', spell_id={0}, _external=True)
         count = row[0]
     return json.dumps(count)
 
-@app.route('/spells/<incantation>')
+@app.route('/spell_count/<incantation>')
 def get_spell_count_by_name(incantation):
     ''' 
     Return the number of time a given spell was used across all books.
@@ -304,7 +350,7 @@ def get_spell_count_by_name(incantation):
 
 
 
-@app.route('/spells/<spell_id>')
+@app.route('/books_spell_count/spell_id/<book_id>/<spell_id>')
 def get_spell_count_by_book(spell_id, book_id):
     ''' 
     Returns the number of time a given spell (input as an ID number) was
@@ -317,11 +363,11 @@ def get_spell_count_by_book(spell_id, book_id):
                 '''.format({0})
 
     for row in _fetch_all_rows_for_query(query):
-        url = flask.url_for('get_spell_count_by_book', spell_id={0}, _external=True)
+        url = flask.url_for('get_spell_by_id', spell_id={0}, _external=True)
         count = row[0]
     return json.dumps(count)
 
-@app.route('/spells/<book_name>/<incantation>')
+@app.route('/books_spell_count/<book_name>/<incantation>')
 def get_spell_count_by_book_by_names(incantation, book_name):
     ''' 
     Return the number of time a given spell was used across a given books.
---
+++ WebApp/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+'''
+    config.py
+    Patty Commins and Anna Meyer, 1 May 2017
+    
+    Database login info for the Harry Potter Spells web 
+    app for CS257, Spring 2017
+'''
+
+database = 'comminsp'
+user = 'comminsp'
+password = 'glass773happy'
