b7dd946ac2cb481c9f30bb51add662c30847f5f6
comminsp <comminsp@carleton.edu>
2017-05-02 23:31:39 -0500

    Added a print statement

--- WebApp/api.py
+++ WebApp/api.py
@@ -391,9 +391,9 @@ def get_spell_count_by_book(spell_id, book_id):
                 WHERE spell_id = {0}
                 AND book_id = {1}
                 '''.format(spell_id)
-
+    print(query)
     for row in _fetch_all_rows_for_query(query):
-        url = flask.url_for('get_spell_by_id', spell_id={1}, _external=True)
+        url = flask.url_for('get_spell_by_id', spell_id={0}, _external=True)
         count = row[0]
     return json.dumps(count)
 
