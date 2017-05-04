cdc48fa2a9c32659d158fd62424fd74d4bcd45b9
comminsp <comminsp@carleton.edu>
2017-05-03 21:41:46 -0500

    Updated tests

--- WebApp/tests/api_tests.py
+++ WebApp/tests/api_tests.py
@@ -48,35 +48,35 @@ class ApiTester(unittest.TestCase):
         self.assertTrue(expected_spell in string_from_server)
 
     #Tests whether our input: book, get: list of spells api query works with Deathly Hallows
-    def bookToSpells1(self):
+    def testBookToSpells1(self):
         url = 'http://thacker.mathcs.carleton.edu:5208/books_spells/Deathly_Hallows'
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
         string_from_server = string_from_server.replace(" ", "").lower()
         expected_list = ["avadakedavra", "caveinimicum", "confringo", "confundus", "crucio", "deprimo", "diffindo", "duro",
                          "engorgio", "erecto", "expectopatronum", "expelliarmus", "expulso", "fidelius","geminio", "glisseo",
-                         "homenumrevelio", "impedimienta", "imperio", "impervius", "levicorpus", "liberacorpus", "lumos",
+                         "homenumrevelio", "impedimenta", "imperio", "impervius", "levicorpus", "liberacorpus", "lumos",
                          "meteolojinxrecanto", "muffliato", "nox", "obliviate", "obscuro", "petrificustotalus",
-                         "piertotumlocomotor", "protego", "protegototalum", "reducio", "relashio", "reparo", "repellomuggletum",
-                         "salviohexia", "sectumsepra", "stuepfy", "tergeo", "wingardiumleviosa"]
+                         "piertotumlocomotor", "protego", "protegototalum", "reducto", "relashio", "reparo", "repellomuggletum",
+                         "salviohexia", "sectumsempra", "stupefy", "tergeo", "wingardiumleviosa"]
         #Test if each spell in expected_list is contained in string from server.
         for spell in expected_list:
             self.assertTrue(spell in string_from_server)
 
     # Tests whether our "input: book, get: list of spells" api query works with Chamber of Secrets
-    def bookToSpells2(self):
+    def testBookToSpells2(self):
         url = 'http://thacker.mathcs.carleton.edu:5208/books_spells/Chamber_of_Secrets'
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
         string_from_server = string_from_server.replace(" ", "").lower()
-        expected_list = ["expelliarmus", "aparecium", "eatslugs", "homorphus", "lumos", "obliviate", "peskipiksipesternomi"
+        expected_list = ["expelliarmus", "aparecium", "eatslugs", "homorphus", "lumos", "obliviate",
                          "rictusempra", "serpensortia", "tarantallegra"]
         #Test if each spell in expected_list is contained in string from server.
         for spell in expected_list:
             self.assertTrue(spell in string_from_server)
 
     #Tests our input: spell, output: purpose/type api query on the spell flagrate
-    def spellToDefinition1(self):
+    def testSpellToDefinition1(self):
         url = "http://thacker.mathcs.carleton.edu:5208/spells/flagrate"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
@@ -90,7 +90,7 @@ class ApiTester(unittest.TestCase):
 
     # Tests our input: spell, output: purpose/type api query on the spell eat_slugs
     # Tests an edge case because this is the last spell in the CSV file
-    def spellToDefinition2(self):
+    def testSpellToDefinition2(self):
         url = "http://thacker.mathcs.carleton.edu:5208/spells/eat_slugs"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
@@ -103,7 +103,7 @@ class ApiTester(unittest.TestCase):
         self.assertEqual(returned_purpose, expected_purpose)
 
     # Tests our api query of input:spell, output: int of how many times the spell was used, with the spell impedimenta
-    def spellToNum1(self):
+    def testSpellToNum1(self):
         url = "http://thacker.mathcs.carleton.edu:5208/spell_count/impedimenta"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
@@ -113,7 +113,7 @@ class ApiTester(unittest.TestCase):
 
     # Tests our api query of input:spell, output: int of how many times the spell was used, with the spell anapneo
     # Tests an edge case because anapneo is only used once
-    def spellToNum2(self):
+    def testSpellToNum2(self):
         url = "http://thacker.mathcs.carleton.edu:5208/spell_count/anapneo"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
@@ -125,7 +125,7 @@ class ApiTester(unittest.TestCase):
 
     # Tests our api query of input:spell, output: int of how many times the spell was used, with the spell accio
     # Tests an edge case because accio is the most frequently used spell.
-    def spellToNum3(self):
+    def testSpellToNum3(self):
         url = "http://thacker.mathcs.carleton.edu:5208/spell_count/accio"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
@@ -136,17 +136,17 @@ class ApiTester(unittest.TestCase):
         self.assertEqual(spell_num, expected_value)
 
     #Tests our input:spell, output: which characters use that spell api query. We test on the spell Accio
-    def spellToCharacters1(self):
+    def testSpellToCharacters1(self):
         url = "http://thacker.mathcs.carleton.edu:5208/characters_by_spell/accio"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
-        expected_char_list = ["Harry Potter", "Molly Weasley", "Fred Weasley", "George Weasley", "Sirius Black",
-                              "Bellatrix Lestrange", "Ron Weasley", "Hermione Granger", "Filius Flitwick"]
+        expected_char_list = ["Harry", "Molly", "Fred", "Sirius",
+                              "Bellatrix", "Ron", "Hermione", "Filius"]
         for char in expected_char_list:
             self.assertTrue(char in string_from_server)
 
     #Tests our input:spell, output: which characters use that spell api query on a spell where there was no known caster
-    def spellToCharacters2(self):
+    def testSpellToCharacters2(self):
         url = "http://thacker.mathcs.carleton.edu:5208/characters_by_spell/conjunctivitis"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
@@ -155,18 +155,18 @@ class ApiTester(unittest.TestCase):
 
     #Tests our input: spell, ourput: which characters use that spell api query on anapneo
     #Edge case because only one caster - Horace Slughorn
-    def spellToCharacters3(self):
+    def testSpellToCharacters3(self):
         url = "http://thacker.mathcs.carleton.edu:5208/characters_by_spell/anapneo"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
         char_list = json.loads(string_from_server)
         self.assertTrue(len(char_list) == 1)
-        expected_char = "Horace Slughorn"
+        expected_char = "Horace"
         self.assertTrue(expected_char in string_from_server)
 
     # tests our api query that takes in a spell and a book and gives the number of occurrences of that spell in that book
     # We test on the Goblet of Fire with the spell Lumos
-    def spellAndBookToNum1(self):
+    def testSpellAndBookToNum1(self):
         url = "http://thacker.mathcs.carleton.edu:5208/books_spell_count/Goblet_of_Fire/Lumos"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
@@ -178,7 +178,7 @@ class ApiTester(unittest.TestCase):
     # tests our api query that takes in a spell and a book and gives the number of occurrences of that spell in that book
     # We test on the Prisoner of Azkaban with the spell tarantallegra.
     # Tests an edge case because tarantallegra is not used in this book
-    def spellAndBookToNum2(self):
+    def testSpellAndBookToNum2(self):
         url = "http://thacker.mathcs.carleton.edu:5208/books_spell_count/Prisoned_of_Azkaban/tarantallegra"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
@@ -191,7 +191,7 @@ class ApiTester(unittest.TestCase):
     # tests our api query that takes in a spell and a book and gives the number of occurrences of that spell in that book
     # We test on the Deathly Hallows with the spell engorgio.
     # Tests an edge case because engorigio is used only once in this book
-    def spellAndBookToNum3(self):
+    def testSpellAndBookToNum3(self):
         url = "http://thacker.mathcs.carleton.edu:5208/books_spell_count/Deathly_Hallows/engorgio"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
@@ -204,8 +204,8 @@ class ApiTester(unittest.TestCase):
     # tests our api query that takes in a spell and a book and gives the number of occurrences of that spell in that book
     # We test on the Prisoner of Azkaban with the spell expecto patronum.
     # Tests an edge case because expecto patronum is used frequently in this book
-    def spellAndBookToNum4(self):
-        url = "http://thacker.mathcs.carleton.edu:5208/books_spell_count/Prisoner_of_Azkaband/expecto_patronum"
+    def testSpellAndBookToNum4(self):
+        url = "http://thacker.mathcs.carleton.edu:5208/books_spell_count/Prisoner_of_Azkaban/expecto_patronum"
         data_from_api = urllib.request.urlopen(url).read()
         string_from_server = data_from_api.decode('utf-8')
         spell_num = json.loads(string_from_server)
